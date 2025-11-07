cbda28845a08e9b40dd4828543287e517873b854
Harshwardhan Singh <harshrao8058@gmail.com>
2025-11-07 08:41:13 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'TODO: Your api key',
-  authDomain: 'TODO: Your auth domain',
-  projectId: 'TODO: Your project id',
-  storageBucket: 'TODO: Your storage bucket',
-  messagingSenderId: 'TODO: Your messaging sender id',
-  appId: 'TODO: Your app id',
+  "projectId": "studio-2807668976-c7652",
+  "appId": "1:461919594799:web:485c01816ab1638f55d9da",
+  "apiKey": "AIzaSyBV1k5eVDN22UiI8B2Zr4JuvWhS8K3tcI0",
+  "authDomain": "studio-2807668976-c7652.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "461919594799"
 };
